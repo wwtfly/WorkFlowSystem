59282b282ed36375970f3835b73d05cab056d049
wwtfly <wwtfly@gmail.com>
2018-01-23 20:49:54 +0800

    update

---
+++ src/main/java/com/ImplDAO/Attend.java
@@ -0,0 +1,9 @@
+package com.ImplDAO;
+
+/**
+ * @Descrintion:员工的考勤记录
+ * @Author: wentao
+ * @Date: Created in 18-1-22 下午10:41
+ */
+public class Attend {
+}
---
+++ src/main/java/com/ImplDAO/Manager.java
@@ -0,0 +1,9 @@
+package com.ImplDAO;
+
+/**
+ * @Descrintion:经理信息
+ * @Author: wentao
+ * @Date: Created in 18-1-22 下午10:43
+ */
+public class Manager {
+}
